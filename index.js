ac331df06e055e604de1a00cea0d3a94e040617a
Mary Oreofejesu Imarenezor <maryimarenezor13@gmail.com>
2024-04-16 22:25:02 -0500

    part 1 completed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+
+app.use('/places', require('./controllers/places'))
 app.get('/', (req, res) => {
     res.send('hello world')
 })
