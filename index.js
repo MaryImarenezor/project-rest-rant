457c6257c6dfcea722c8575a1ae2a9a2a82619b9
Mary Oreofejesu Imarenezor <maryimarenezor13@gmail.com>
2024-04-16 22:17:44 -0500

    adding 404 page route

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 app.get('/', (req, res) => {
     res.send('hello world')
 })
+app.get('*', (req, res) => {
+    res.send('<h1>404 Page</h1>').status(404)
+})
 
-app.listen(3000)
+
+app.listen(process.env.PORT)
