cc75cca30154e70c9de365d74fcd2ea9684df7f0
Mary Oreofejesu Imarenezor <maryimarenezor13@gmail.com>
2024-05-13 18:41:57 -0500

    default.jsx edited

--- views/default.jsx
+++ views/default.jsx
@@ -7,6 +7,7 @@ function Def(html) {
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="./css/style.css" />
+                <link rel="stylesheet" href="../css/style.css" />
             </head>
             <body>
                 <nav>
