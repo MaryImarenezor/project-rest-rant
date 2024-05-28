5858304e2db10f24001cfcba04ff1a67b721ad78
Mary Oreofejesu Imarenezor <maryimarenezor13@gmail.com>
2024-05-28 18:29:47 -0500

    part 10 completed

--- controllers/places.js
+++ controllers/places.js
@@ -23,21 +23,10 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    /*
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-        res.render('places/show', {place: places[id], id})
-    }
-    */
-    //res.send('GET /places/:id stub')
     db.Place.findById(req.params.id)
+    .populate('comments')
     .then(place => {
+        console.log(place.comments)
         res.render('places/show', { place })
     })
     .catch(err => {
@@ -45,6 +34,7 @@ router.get('/:id', (req, res) => {
         res.render('error404')
     })
 })
+
 
 router.delete('/:id', (req, res) => {
     db.Place.findByIdAndDelete(req.params.id)
@@ -144,6 +134,30 @@ router.put('/:id', (req, res) => {
         console.log(err);
         res.render('error404');
     });
+})
+
+router.get('/:id/comment', (req, res) => {
+    res.render('places/newcomment')
+})
+router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    db.Place.findById(req.params.id)
+    .then(place => {
+        db.Comment.create(req.body)
+        .then(comment => {
+            place.comments.push(comment.id)
+            place.save()
+            .then(() => {
+                res.redirect(`/places/${req.params.id}`)
+            })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+    })
+    .catch(err => {
+        res.render('error404')
+    })
 })
 
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,27 @@ const React = require('react')
 const Def = require("../default")
 
 function show(data) {
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+        return (
+            <div className="border">
+                <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😍'}</h2>
+                <h4>{c.content}</h4>
+                <h3>
+                    <stong>- {c.author}</stong>
+                </h3>
+                <h4>Rating: {c.stars}</h4>
+            </div>
+        )
+        })
+    }
+
     return (
         <Def>
             <main>
@@ -27,7 +48,7 @@ function show(data) {
                         <h4>
                             Serving {data.place.cuisines}
                         </h4>
-                        
+
                         <a href="" className="btn btn-warning">Edit</a>
                         <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
                             <button href={`/places/${data.id}/edit`} className='btn btn-danger' type='submit'>Delete</button>
@@ -36,7 +57,9 @@ function show(data) {
                     </div>
 
                     <h2>Comments</h2>
-                    <p>no comments yet!</p>
+                    {comments}
+
+
                 </div>
             </main>
         </Def>
@@ -50,3 +73,6 @@ function show(data) {
 
 
 module.exports = show
+
+
+
